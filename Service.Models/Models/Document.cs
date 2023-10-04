91dbcff30163df1c3e8635610dd89fa337b793f3
Nikhil Kumar <nkhlneil@gmail.com>
2023-10-04 15:35:00 +0200

    Added model

---
+++ Service.Models/Models/Document.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Service.Models.Models
+{
+    public class Document
+    {
+        [Key]
+        public int DocumentId { get; set; }
+        [Required]
+        public required string Type { get; set; }
+        [Required]
+        public required byte[] Data { get; set; }
+        [Required]
+        public int HistoryNo { get; set; }
+        public DateTime? ExportDateTime { get; set; }
+        [Required]
+        public bool Processed { get; set; }
+        public byte[]? ProcessedData { get; set; }
+        public string? ProcessedFilename { get; set; }
+        public string? ProcessedFiletype { get; set; }
+        public string? ProcessedFreetext { get; set; }
+    }
+}

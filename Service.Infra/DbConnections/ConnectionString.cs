a43eb0850bdcd609687b7d0c9b8316593ec6c046
Nikhil Kumar <nkhlneil@gmail.com>
2023-10-04 16:40:46 +0200

    Added infra project for db connections

---
+++ Service.Infra/DbConnections/ConnectionString.cs
@@ -0,0 +1,29 @@
+﻿namespace Service.Infra.DbConnections
+{
+    public class ConnectionString
+    {
+        /// <summary>
+        /// Read database and table name from environment and
+        /// creates an SQL connection string based on it. Uses
+        /// windows authentication for connecting to database.
+        /// Throws if mandatory environment variables for db
+        /// name and table name are not provided.
+        /// </summary>
+        public static string Create()
+        {
+            var dbName = Environment.GetEnvironmentVariable("CONNECTION_DB_NAME")
+                ?? throw new ArgumentNullException("Mandatory environment variable", "CONNECTION_DB_NAME");
+            var tableName = Environment.GetEnvironmentVariable("CONNECTION_DB_TABLE")
+                ?? throw new ArgumentNullException("Mandatory environment variable", "CONNECTION_DB_TABLE");
+
+            return $"Server=tcp:{dbName};" 
+                + $"Initial Catalog={tableName};"
+                + "Persist Security Info=False;"
+                + "Integrated Security=SSPI;"
+                + "MultipleActiveResultSets=False;"
+                + "Encrypt=True;"
+                + "TrustServerCertificate=True;"
+                + "Connection Timeout=30;";
+        }
+    }
+}
